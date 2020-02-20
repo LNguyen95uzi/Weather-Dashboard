a37d27344b717c71e483a304a8c490423f488bb6
Long Nguyen <Long.Nguyen@temple.edu>
2020-02-19 23:12:11 -0500

    Multiple Edits

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Weather-Dashboard
+
+Deployed app: https://lnguyen95uzi.github.io/Weather-Dashboard/
+
+Project Description:
+
+This app, with the help of OpenWeather API, helps the user search by city name and receives current weather conditions for that city, including temperature, humidity, wind speed, and UV index, and an icon that represents the current weather (sunny, cloudy, rain, snow,...). Along with the current day, a 5-day forecast is also provided for said city providing the temperature and humidity. 
+
+As the user clicks to search a city, that city is saved in local storage and shown under the search box. 
+
+Challenges:
+Many challenges were encountered along the way. Javascript is still hard to reformat to get right. Issues with building local storage and ajax/api system.
+
+Installation: None Required
+
+Credits: None
+
+License: None
+
+HTML and CSS used for structure and style.
--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 // Javascript here
+function homePage() {
+    let cityEl = document.getElementById("city-input");
+    let searchEl = document.getElementById("search-button");
+    let nameEl = document.getElementById("city-name");
+    let currentIconEl = document.getElementById("current-icon");
+    let currentTempEl = document.getElementById("temperature");
+    let currentHumEl = document.getElementById("humidity");
+    let currentWindEl = document.getElementById("wind-speed");
+    let currentUVEl = document.getElementById("uv-index");
+    let historyEl = document.getElementById("history");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+    // console.log(searchHistory);
+
+    // API Key
+    let APIkey = "f812bd0ab380498b5ba21900ab4d5a02";
+
+    // Building URL needed to query database
+    function weather(cityName) {
+
+
+        let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + APIkey;
+
+        // Run AJAX call to OpenWeatherMap API
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            // Store retrieved data inside "response"
+            .then(function (response) {
+                console.log(queryURL);
+                console.log(response);
+            })
+
+        searchEl.addEventListener("click", function () {
+            let searchValue = cityEl.value;
+            weather(searchValue);
+            searchHistory.push(searchValue);
+            localStorage.setItem("search", JSON.stringify(searchHistory));
+            getSearchHistory();
+        })
+
+    }
+    function tempF(K) {
+        return Math.floor((K - 273.15) * 1.80 + 32);
+    }
+
+    function getSearchHistory() {
+        historyEl.innerHTML = "";
+        for (let i = 0; i < searchHistory.length; i++) {
+            const historySearch = document.createElement("input");
+            historySearch.setAttribute("type", "text");
+            historySearch.setAttribute("readonly", true);
+            historySearch.setAttribute("class", "form-control d-block bg-white");
+            historySearch.setAttribute("value", searchHistory[i]);
+            historySearch.addEventListener("click", function () {
+                weather(historySearch.value);
+            })
+            historyEl.append(historySearch);
+        }
+    }
+    getSearchHistory();
+    if (searchHistory.length > 0) {
+        weather(searchHistory[searchHistory.length - 1]);
+    }
+}
+homePage();
